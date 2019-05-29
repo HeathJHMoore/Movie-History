aad2e70958775752e106ea945157fa3b53d2a150
Heath JH Moore <heathjhmoore@gmail.com>
2019-05-28 21:19:16 -0500

    added styling to cards

--- src/javascripts/components/Movies/movies.js
+++ src/javascripts/components/Movies/movies.js
@@ -8,9 +8,13 @@ const movieBuilder = () => {
       console.error('this is the moview Array', moviesArray);
       let domString = '';
       moviesArray.forEach((movie) => {
-        domString += '<div class="card col-3">';
+        domString += '<div class="card col-6 col-lg-3 p-0 text-center">';
         domString += `<h2>${movie.title}</h2>`;
-        domString += `<img src="${movie.imageURL}">`;
+        domString += `<img src="${movie.imageURL}" height="300px">`;
+        domString += '<div>';
+        domString += '<button class="btn btn-primary w-100" style="border: solid 1px black; border-radius: 0px;">Add to Watchlist</button>';
+        domString += '<button class="btn btn-secondary w-100" style="border: solid 1px black; border-radius: 0px;">Rate this Movie</button>';
+        domString += '</div>';
         domString += '</div>';
         util.printToDom('movies', domString);
       });
